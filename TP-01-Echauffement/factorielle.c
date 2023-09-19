7d203cd047e8c89e7c9e2f87eced7e71fbf29b32
Charreyron Anthony <charreyronanthony0263@gmail.com>
2023-09-19 15:16:38 +0200

    Exercice factorielle

--- TP-01-Echauffement/factorielle.c
+++ TP-01-Echauffement/factorielle.c
@@ -2,15 +2,15 @@
 #include <stdlib.h> // EXIT_SUCESS
 #include <stdbool.h> // bool
 #include <stdint.h> // uint8_t, uint64_t, ... entier non signé codé sur 8 ou 64 bits
+#include <inttypes.h>
 
-typedef short TypeEntier;
+typedef unsigned long long TypeEntier;
 TypeEntier factorielle(TypeEntier);
 
 int main (void){
     for (int i=1; i<16;i++){
-        printf("La factorielle de %u est : %u   \n", i, factorielle(i));
+        printf("La factorielle de %u est : %" PRIu64  "\n", i, factorielle(i));
     }
-
 }
 TypeEntier factorielle(TypeEntier n){
     TypeEntier prod =1;
@@ -19,3 +19,8 @@ TypeEntier factorielle(TypeEntier n){
     }
     return prod;
 }
+
+// Après quelques calcul de factorielles, le nombre obtenu est trop grand et n'est plus affiché
+//Le type de TypeEntier ne permet pas d'afficher des aussi grands nombres
+
+//Après changement de type, les résultats s'affichent correctement
