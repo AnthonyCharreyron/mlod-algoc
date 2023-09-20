48a5a95dabf1d632fbb7adf62e5e2fd4bd9debb6
Charreyron Anthony <charreyronanthony0263@gmail.com>
2023-09-20 10:56:41 +0200

    Exercice matrice

---
+++ TP-02-pointeurs/01-echangeContenu/matrice.c
@@ -0,0 +1,38 @@
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <inttypes.h>
+
+#define SIZE 5
+
+void matrix_mult (int64_t matriceResultat[][SIZE],int64_t matrice1[][SIZE],int64_t matrice2[][SIZE]){
+    for (int i=0; i<SIZE;i++){
+        for(int j=0; j<SIZE;j++){
+            int64_t somme=0;
+            for (int k = 0; k<SIZE;k++){
+                somme+= matrice1[i][k]*matrice2[k][j];
+            }
+            matriceResultat[i][j]=somme;
+        }
+    }
+}
+
+void matrix_print(int64_t matrice[SIZE][SIZE]){
+
+    for (int i=0; i<SIZE;i++){
+        for(int j=0; j<SIZE;j++){
+            printf(" %" PRId64 "\t",matrice[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main(void){
+    int64_t matrice1[][SIZE]={{1,2,3,4,5},{1,2,3,4,5},{1,2,3,4,5},{1,2,3,4,5},{1,2,3,4,5}};
+    int64_t matrice2[][SIZE]={{6,7,8,9,10},{6,7,8,9,10},{6,7,8,9,10},{6,7,8,9,10},{6,7,8,9,10}};
+    int64_t matriceResultat[SIZE][SIZE];
+    matrix_mult(matriceResultat,matrice1,matrice2);
+    matrix_print(matriceResultat);
+    return EXIT_SUCCESS;
+}
+// b) mult_mattrix est utilisable que pour les matrices de taille SIZE,SIZE
