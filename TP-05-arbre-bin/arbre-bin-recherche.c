21974005b003acd4cc4434fa615779e91ce011cd
Charreyron Anthony <charreyronanthony0263@gmail.com>
2023-10-04 12:08:17 +0200

    Début de TP

--- TP-05-arbre-bin/arbre-bin-recherche.c
+++ TP-05-arbre-bin/arbre-bin-recherche.c
@@ -9,46 +9,109 @@ bool estVide(ArbreBinaire a) {
 
 // initialise l'arbre a à l'arbre vide
 void initialiser(ArbreBinaire* a) {
+	*a = NULL;
 }
 
 // créer un arbre contenant un seul noeud ayant la valeur e
 ArbreBinaire creer(Element e) {
-
-	return NULL;
+	ArbreBinaire new=malloc(sizeof(Noeud));
+	new->val=e;
+	new->filsGauche=NULL; // equivalent à initialiser(&new->filsGauche)
+	new->filsDroit=NULL;
+	return new;
 }
 
 // insere e dans a sachant que a est un arbre binaire de recherche
 // si a contient déjà un element e, ce dernier n'est pas insérer afin d'éviter les doublons
 // version itérative
 ArbreBinaire insere_i(ArbreBinaire a, Element e) {
-
-	return NULL;
+	ArbreBinaire parcours=a;
+	while(!estVide(a)){
+		ArbreBinaire gauche=parcours->filsGauche;
+		ArbreBinaire droite=parcours->filsDroit;
+		if (e == parcours->val){
+			return a;
+		}
+		else if ( e < gauche->val){
+			parcours=gauche;
+		}
+		else {
+			parcours=droite;
+		}
+	}
+	parcours=creer(e);
+	return a;
 }	
 
 // insere e dans a sachant que a est un arbre binaire de recherche
 // si a contient déjà un element e, ce dernier n'est pas insérer afin d'éviter les doublons
 // version récursive
 ArbreBinaire insere_r(ArbreBinaire a, Element e) {
-	
-	return NULL;
-}
+	if(!estVide(a)){
+		ArbreBinaire gauche=a->filsGauche;
+		ArbreBinaire droite=a->filsDroit;
+		if (e == a->val){
+			return a;
+		}
+		else if ( e < gauche->val){
+			a->filsGauche=insere_r(gauche,e);
+		}
+		else {
+			a->filsDroit=insere_r(droite,e);
+		}
+	}
+	a=creer(e);
+	return a;
+	}
+
 
 // retourne le nombre de noeud contenus dans l'arbre a
 int nombreDeNoeud(ArbreBinaire a){
-
+	if (estVide(a)){
 		return 0;
+	}
+	return 1 + nombreDeNoeud(a->filsGauche) + nombreDeNoeud(a->filsDroit);
 }
-
 
 // retourne la profondeur du noeud ayant la valeur e dans a
 // retourne -1 si a est vide ou si e n'est pas dans a
 int profondeur(ArbreBinaire a, Element e){
-
-		return 0;
+	if (estVide(a)){
+		return -1;
+	}
+	int profondeur = 1;
+	ArbreBinaire parcours=a;
+	while(!estVide(a)){
+		ArbreBinaire gauche=parcours->filsGauche;
+		ArbreBinaire droite=parcours->filsDroit;
+		if (e == parcours->val){
+			return profondeur;
+		}
+		else if ( e < gauche->val){
+			parcours=gauche;
+		}
+		else {
+			parcours=droite;
+		}
+		profondeur++;
+	}
+	return -1;
 }
 
 // retourne la hauteur de l'arbre a
 int hauteur(ArbreBinaire a){
+	if (estVide(a)){
+		return -1;
+	}
+	int max=a->val;
+	ArbreBinaire parcours=a;
+	// while(!estVide(a)){
+	// 	ArbreBinaire gauche=parcours->filsGauche;
+	// 	ArbreBinaire droite=parcours->filsDroit;
+	// 	if() {
+	// 		parcours=droite;
+	// 	}
+	// }
 	return 0;
 }
 
@@ -59,13 +122,14 @@ ArbreBinaire pere(ArbreBinaire a, Element elem){
 }
 
 
-void afficheRGD_r(ArbreBinaire a){
+void afficheRGD_r(ArbreBinaire a){ //Afficher Racine Gauche Droite
+	
 }
 
-void afficheGRD_r(ArbreBinaire a){
+void afficheGRD_r(ArbreBinaire a){ //Afficher Gauche Racine Droite
 }
 
-void afficheGDR_r(ArbreBinaire a){
+void afficheGDR_r(ArbreBinaire a){ //Afficher Gauche Droite Racine
 }
 
 // retourne le noeud dont la valeur est minimum dans l'arbre
